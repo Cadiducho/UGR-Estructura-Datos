397e068aa80786e2758a7eed50d1390248492318
Cadiducho <cadiducho@gmail.com>
2018-01-10 17:57:25 +0100

    Readme y práctica final

--- Practica5/AVL/usoAVL.cpp
+++ Practica5/AVL/usoAVL.cpp
@@ -1,98 +1,56 @@
-#include <iostream>
-#include "AVL.hpp"
+#include "avl.h"
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <vector>
 using namespace std;
 
+void ListarAVL(AVL<int> &ab_bus){
+  AVL<int>::nodo n;
+ cout<<endl<<" Elementos ordenados con sus alturas: ";
+ 
+ for (n=ab_bus.begin();n!=ab_bus.end();++n){
+	   cout<<*n<<" ("<<n.altura()<< ") ";
+ }
+}
+int main(){
+ vector<int>v;
+ int a;
+ while (cin>>a){
+	   v.push_back(a);
+ }
+cin.clear(); 
+ AVL<int>ab_bus;
+ 
+ for (int i=0;i<v.size();i++){
 
-int main(int argc, const char * argv[]) {
-  AVL<int> avl;
-  AVL<int> avl2;
-  AVL<int>::iterator it;
-  AVL<int> unionavl;
-  AVL<int> interseccion;
-  int e;
+	   ab_bus.Insertar(v[i]);
+	   cout<<endl<<"Insetando "<<v[i];
+	   ListarAVL(ab_bus);
 
-  cout << "INSERCIÓN DE DATOS" << endl;
+ }
+ 
+ //ListarAVL(ab_bus);
+    
+int x;
+bool salir=false;
+cout<<endl<<"Dime un elemento a borrar: ";
+while (!salir && cin>>x){
+	 if (ab_bus.Buscar(x)){
+	  //cout<<endl<<x<<" esta"<<endl;
+	  ab_bus.Borrar(x);
+	  
+	  }
+	  else{ 
+	    cout<<"El elemento "<<x<<" no esta"<<endl;
+	  }
+	  
+	  if (!ab_bus.empty()){
+	   ListarAVL(ab_bus);	    
+	   cout<<endl<<"Dime un elemento a borrar: ";
+	  } 
+	  else salir=true;
+}
 
-  cout << "Introduce un entero para AVL 1 (<0 para terminar) ";
-  cin >> e;
-  while(e>=0){
-    avl.insertar(e);
-    for(it = avl.begin(); it != avl.end(); ++it)
-      cout << *it << " ";
-    cout << endl;
-    avl.Esquema();
-
-    cout << "Introduce un entero AVL 1 (<0 para terminar) ";
-    cin >> e;
-  }
-
-  cout << "Introduce un entero para AVL 2 (<0 para terminar) ";
-  cin >> e;
-  while(e>=0){
-    avl2.insertar(e);
-    for(it = avl2.begin(); it != avl2.end(); ++it)
-      cout << *it << " ";
-    cout << endl;
-    avl2.Esquema();
-
-    cout << "Introduce un entero AVL 2 (<0 para terminar) ";
-    cin >> e;
-  }
-
-//  const int N = 50;
-//  const float max = 100.0;
-//  srand(time(NULL));
-//  for(int i=0; i<N; i++)
-//    avl.insertar((int) (max*rand()/RAND_MAX));
-//
-//
-//  for(it = avl.begin(); it!=avl.end(); ++it)
-//    cout << *it << " ";
-//  cout << endl;
-//
-//  avl.Esquema();
-/*
-  cout << "BÚSQUEDA DE DATOS" << endl;
-  cout << "Introduce un entero (< 0 para terminar) ";
-  cin >> e;
-  while(e>=0){
-    cout << e << (avl.existe(e)? " SÍ" : " NO") << " está en el AVL" << endl;
-    cout << "Introduce un entero (< 0 para terminar) ";
-    cin >> e;
-  }
-
-  avl.Esquema();
-  cout << "BORRADO DE DATOS" << endl;
-  cout << "Introduce un entero (< 0 para terminar) ";
-  cin >> e;
-  while(e>=0){
-    avl.borrar(e);
-    for(it = avl.begin(); it!=avl.end(); ++it)
-      cout << *it << " ";
-    cout << endl;
-    avl.Esquema();
-
-    cout << "Introduce un entero (< 0 para terminar) ";
-    cin >> e;
-}*/
-
-  for (it = avl.begin(); it != avl.end(); ++it) {
-    if (!unionavl.existe(*it))
-      unionavl.insertar(*it);
-  }
-  for (it = avl2.begin(); it != avl2.end(); ++it) {
-    if (!unionavl.existe(*it))
-      unionavl.insertar(*it);
-  }
-  cout << "Esquema de la union" << endl;
-  unionavl.Esquema();
-
-  for (it = avl.begin(); it != avl.end(); ++it) {
-    if (avl2.existe(*it))
-      interseccion.insertar(*it);
-  }
-  cout << "Esquema de la interseccion" << endl;
-  interseccion.Esquema();
-
-  return 0;
+ 
 }
---
+++ README.md
@@ -0,0 +1,13 @@
+# Estructura de Datos
+
+Prácticas realizadas en la asignatura ED, curso 2017-2018, en la Universidad de Granada (UGR)
+
+## Indice
+ * Práctica 1: Eficiencia
+ * Práctica 2: Abstracción
+ * Práctica 3: Lineales
+ * Práctica 4: STL
+ * Práctica 5: Árboles
+ * Práctica Final: Quién es Quién
+---
+[@Cadiducho](https://twitter.com/Cadiducho)
